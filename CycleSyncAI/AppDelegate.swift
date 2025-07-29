26f3b0a09988dc4b3c04e29f91a2b168ff5899cb
medhini98 <59174666+medhini98@users.noreply.github.com>
2025-07-29 23:47:56 +0530

    Fix variable name conflict

--- CycleSyncAI/AppDelegate.swift
+++ CycleSyncAI/AppDelegate.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import HealthKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Nothing to do here yet; permissions are requested in LaunchViewController
+
         return true
     }
 
